f91b2d040ed78cef244d07dee8628bfa8b800c24
Dmytro Aleksieiev <dmitriy.aleksieiev@gmail.com>
2024-04-25 09:34:56 +0300

    2504930

---
+++ script.js
@@ -0,0 +1,11 @@
+// При кліку на кнопку "Подвоїти" збільшувати значення в кожному
+// елементі списку в два рази.
+
+// const listItemEl = document.querySelectorAll(".listItem");
+// const buttonEl = document.querySelector("#double");
+
+// buttonEl.addEventListener("click", onClick);
+
+// function onClick() {
+//   listItemEl.forEach((item) => (item.textContent = item.textContent * 2));
+// }
--- scrypt.js
+++
@@ -1,4 +0,0 @@
-const text = document.querySelector(".js-text");
-console.log("🚀 ~ text:", text);
-const cutText = text.textContent.trim().slice(0, 3);
-console.log("🚀 ~ cutText:", cutText);
